4c3d1c30996738c0c772f6299eeb5d37c2f14237
bdolor <bpayne@bccampus.ca>
2017-11-21 17:11:54 -0800

    extending epub201 instead, override parent functions

--- inc/export/cc/class-imscc11.php
+++ inc/export/cc/class-imscc11.php
@@ -14,12 +14,13 @@
 
 namespace BCcampus\Export\CC;
 
-use \PressBooks\Modules\Export\Export;
+
 use \PressBooks;
+use Pressbooks\Modules\Export\Epub\Epub201;
 use \Pressbooks\Sanitize;
 use \Pressbooks\HtmLawed;
 
-class Imscc11 extends Export {
+class Imscc11 extends Epub201 {
 	/**
 	 * Temporary directory used to build IMSCC
 	 *
@@ -37,6 +38,7 @@ class Imscc11 extends Export {
 	 * @var array
 	 */
 	protected $manifest = [];
+	protected $fetchedMediaCache = [];
 
 	/**
 	 * Imscc11 constructor.
@@ -102,93 +104,55 @@ class Imscc11 extends Export {
 		// Reset manifest
 		$this->manifest = [];
 
-		echo "<pre>";
-		print_r( get_defined_vars() );
-		echo "</pre>";
-		die();
-//		$this->createFrontMatter( $book_contents, $metadata );
+		$this->createFrontMatter( $book_contents, $metadata );
 //		$this->createPartsAndChapters( $book_contents, $metadata );
 //		$this->createBackMatter( $book_contents, $metadata );
 
 	}
 
-	/**
-	 * Making it xml friendly
-	 * Copied verbatim from class-epub201.php in pressbooks
-	 * @copyright Pressbooks
-	 *
-	 * @param $book_contents
-	 *
-	 * @return mixed
-	 */
-	protected function preProcessBookContents( $book_contents ) {
+	protected function createFrontMatter( $book_contents, $metadata ) {
 
-		// We need to change global $id for shortcodes, the_content, ...
-		global $id;
-		$old_id = $id;
+		echo "<pre>";
+		print_r( get_defined_vars() );
+		echo "</pre>";
+		die();
 
-		// Do root level structures first.
-		foreach ( $book_contents as $type => $struct ) {
-
-			if ( preg_match( '/^__/', $type ) ) {
-				continue; // Skip __magic keys
-			}
-
-			foreach ( $struct as $i => $val ) {
-
-				if ( isset( $val['post_content'] ) ) {
-					$id                                           = $val['ID'];
-					$book_contents[ $type ][ $i ]['post_content'] = $this->preProcessPostContent( $val['post_content'] );
-				}
-				if ( isset( $val['post_title'] ) ) {
-					$book_contents[ $type ][ $i ]['post_title'] = Sanitize\sanitize_xml_attribute( $val['post_title'] );
-				}
-				if ( isset( $val['post_name'] ) ) {
-					$book_contents[ $type ][ $i ]['post_name'] = $this->preProcessPostName( $val['post_name'] );
-				}
-
-				if ( 'part' === $type ) {
-
-					// Do chapters, which are embedded in part structure
-					foreach ( $book_contents[ $type ][ $i ]['chapters'] as $j => $val2 ) {
-
-						if ( isset( $val2['post_content'] ) ) {
-							$id                                                             = $val2['ID'];
-							$book_contents[ $type ][ $i ]['chapters'][ $j ]['post_content'] = $this->preProcessPostContent( $val2['post_content'] );
-						}
-						if ( isset( $val2['post_title'] ) ) {
-							$book_contents[ $type ][ $i ]['chapters'][ $j ]['post_title'] = Sanitize\sanitize_xml_attribute( $val2['post_title'] );
-						}
-						if ( isset( $val2['post_name'] ) ) {
-							$book_contents[ $type ][ $i ]['chapters'][ $j ]['post_name'] = $this->preProcessPostName( $val2['post_name'] );
-						}
-					}
-				}
-			}
-		}
-
-		$id = $old_id;
-
-		return $book_contents;
 	}
 
 	/**
+	 * Nearly Verbatim from class-epub201.php in pressbooks v4.4.0
+	 * @copyright Pressbooks
 	 *
-	 * @param string $content
+	 * @param \DOMDocument $doc
 	 *
-	 * @return string
+	 * @return \DOMDocument
 	 */
-	protected function preProcessPostContent( $content ) {
+	protected function scrapeAndKneadImages( \DOMDocument $doc ) {
 
-		$content = apply_filters( 'the_content', $content );
-		$content = $this->tidy( $content );
+		$fullpath = $this->tmpDir . '/webcontent/assets';
 
-		return $content;
+		$images = $doc->getElementsByTagName( 'img' );
+		foreach ( $images as $image ) {
+			/** @var \DOMElement $image */
+			// Fetch image, change src
+			$url      = $image->getAttribute( 'src' );
+			$filename = $this->fetchAndSaveUniqueImage( $url, $fullpath );
+			if ( $filename ) {
+				// Replace with new image
+				$image->setAttribute( 'src', 'assets/' . $filename );
+			} else {
+				// Tag broken image
+				$image->setAttribute( 'src', "{$url}#fixme" );
+			}
+		}
+
+		return $doc;
 	}
 
 	/**
 	 * Tidy HTML
-	 * Copied verbatim from class-epub201.php in pressbooks
+	 * Verbatim from class-epub3.php in pressbooks v4.4.0
+	 * @copyright Pressbooks
 	 *
 	 * @param string $html
 	 *
@@ -196,17 +160,52 @@ class Imscc11 extends Export {
 	 */
 	protected function tidy( $html ) {
 
-		// Make XHTML 1.1 strict using htmlLawed
+		// Venn diagram join between XTHML + HTML5 Deprecated Attributes
+		//
+		// Our $spec is artisanally hand crafted based on squinting very hard while reading the following docs:
+		//
+		//  + 2.3 - Extra HTML specifications using the $spec parameter
+		//  + 3.4.6 -  Transformation of deprecated attributes
+		//  + 3.3.2  - Tag-transformation for better compliance with standards
+		//  + HTML5 - Deprecated Tags & Attributes
+		//
+		// That is we do not remove deprecated attributes that are already transformed by htmLawed
+		//
+		// More info:
+		//  + http://www.bioinformatics.org/phplabware/internal_utilities/htmLawed/beta/htmLawed_README.htm
+		//  + http://www.tutorialspoint.com/html5/html5_deprecated_tags.htm
 
 		$config = [
 			'valid_xhtml'        => 1,
 			'no_deprecated_attr' => 2,
 			'unique_ids'         => 'fixme-',
-			'deny_attribute'     => 'itemscope,itemtype,itemref,itemprop',
-			'hook'               => '\Pressbooks\Sanitize\html5_to_xhtml11',
+			'hook'               => '\Pressbooks\Sanitize\html5_to_epub3',
 			'tidy'               => - 1,
+			'make_tag_strict'    => 2,
 			'comment'            => 1,
 		];
+
+		$spec = '';
+		$spec .= 'a=,-charset,-coords,-rev,-shape;';
+		$spec .= 'area=-nohref;';
+		$spec .= 'col=-align,-char,-charoff,-valign,-width;';
+		$spec .= 'colgroup=-align,-char,-charoff,-valign,-width;';
+		$spec .= 'div=-align;';
+		$spec .= 'iframe=-align,-frameborder,-longdesc,-marginheight,-marginwidth,-scrolling;';
+		$spec .= 'img=-longdesc;';
+		$spec .= 'link=-charset,-rev,-target;';
+		$spec .= 'menu=-compact;';
+		$spec .= 'object=-archive,-classid,-codebase,-codetype,-declare,-standby;';
+		$spec .= 'param=-type,-valuetype;';
+		$spec .= 't=-abbr,-axis;';
+		$spec .= 'table=-border,-cellpadding,-frame,-rules;';
+		$spec .= 'tbody=-align,-char,-charoff,-valign;';
+		$spec .= 'td=-axis,-abbr,-align,-char,-charoff,-scope,-valign;';
+		$spec .= 'tfoot=-align,-char,-charoff,-valign;';
+		$spec .= 'th=-align,-char,-charoff,-valign;';
+		$spec .= 'thead=-align,-char,-charoff,-valign;';
+		$spec .= 'tr=-align,-char,-charoff,-valign;';
+		$spec .= 'ul=-type;';
 
 		// Reset on each htmLawed invocation
 		unset( $GLOBALS['hl_Ids'] );
@@ -214,7 +213,9 @@ class Imscc11 extends Export {
 			$GLOBALS['hl_Ids'] = $this->fixme;
 		}
 
-		return HtmLawed::filter( $html, $config );
+		$html = HtmLawed::filter( $html, $config, $spec );
+
+		return $html;
 	}
 
 	/**
@@ -239,4 +240,112 @@ class Imscc11 extends Export {
 	function validate() {
 		// TODO: Implement validate() method.
 	}
+
+	/**
+	 * Nearly verbatim from class-epub3.php in pressbooks v4.4.0
+	 * @copyright Pressbooks
+	 *
+	 * @param \DOMDocument $doc
+	 *
+	 * @return \DOMDocument
+	 */
+	protected function scrapeAndKneadMedia( \DOMDocument $doc ) {
+		$fullpath = $this->tmpDir . '/webcontent/assets';
+		$tags     = [ 'source', 'audio', 'video' ];
+
+		foreach ( $tags as $tag ) {
+
+			$sources = $doc->getElementsByTagName( $tag );
+			foreach ( $sources as $source ) {
+				/** @var $source \DOMElement */
+				if ( ! empty( $source->getAttribute( 'src' ) ) ) {
+					// Fetch the audio file
+					$url      = $source->getAttribute( 'src' );
+					$filename = $this->fetchAndSaveUniqueMedia( $url, $fullpath );
+
+					if ( $filename ) {
+						// Change src to new relative path
+						$source->setAttribute( 'src', 'assets/' . $filename );
+					} else {
+						// Tag broken media
+						$source->setAttribute( 'src', "{$url}#fixme" );
+					}
+				}
+			}
+		}
+
+		return $doc;
+	}
+
+	/**
+	 * Nearly Verbatim from class-epub3.php in pressbooks v4.4.0
+	 * @copyright Pressbooks
+	 *
+	 * @param $url
+	 * @param $fullpath
+	 *
+	 * @return array|mixed|string
+	 */
+	protected function fetchAndSaveUniqueMedia( $url, $fullpath ) {
+
+		if ( isset( $this->fetchedMediaCache[ $url ] ) ) {
+			return $this->fetchedMediaCache[ $url ];
+		}
+
+		$response = wp_remote_get( $url, [ 'timeout' => $this->timeout ] );
+
+		// WordPress error?
+		if ( is_wp_error( $response ) ) {
+			try {
+				// protocol relative urls handed to wp_remote_get will fail
+				// try adding a protocol
+				$protocol_relative = wp_parse_url( $url );
+				if ( ! isset( $protocol_relative['scheme'] ) ) {
+					if ( true === is_ssl() ) {
+						$url = 'https:' . $url;
+					} else {
+						$url = 'http:' . $url;
+					}
+				}
+				$response = wp_remote_get( $url, [ 'timeout' => $this->timeout ] );
+				if ( is_wp_error( $response ) ) {
+					throw new \Exception( 'Bad URL: ' . $url );
+				}
+			} catch ( \Exception $exc ) {
+				$this->fetchedImageCache[ $url ] = '';
+				error_log( '\BCcampus\Export\Imscc wp_error on wp_remote_get() - ' . $response->get_error_message() . ' - ' . $exc->getMessage() );
+
+				return '';
+			}
+		}
+
+		// Basename without query string
+		$filename = explode( '?', basename( $url ) );
+		$filename = array_shift( $filename );
+		$filename = explode( '#', $filename )[0]; // Remove trailing anchors
+		$filename = sanitize_file_name( urldecode( $filename ) );
+		$filename = Sanitize\force_ascii( $filename );
+
+		$tmp_file = \Pressbooks\Utility\create_tmp_file();
+		file_put_contents( $tmp_file, wp_remote_retrieve_body( $response ) );
+
+		if ( ! \Pressbooks\Media\is_valid_media( $tmp_file, $filename ) ) {
+			$this->fetchedMediaCache[ $url ] = '';
+
+			return ''; // Not a valid media type
+		}
+
+		// Check for duplicates, save accordingly
+		if ( ! file_exists( "$fullpath/$filename" ) ) {
+			copy( $tmp_file, "$fullpath/$filename" );
+		} elseif ( md5( file_get_contents( $tmp_file ) ) !== md5( file_get_contents( "$fullpath/$filename" ) ) ) {
+			$filename = wp_unique_filename( $fullpath, $filename );
+			copy( $tmp_file, "$fullpath/$filename" );
+		}
+
+		$this->fetchedMediaCache[ $url ] = $filename;
+
+		return $filename;
+	}
+
 }
