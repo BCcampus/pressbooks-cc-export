e4e2316974256d1ec437f9a4423a46406e22918a
bdolor <bpayne@bccampus.ca>
2017-11-23 16:19:43 -0800

    remove duplicate files in zip generation

--- inc/export/cc/class-imscc11.php
+++ inc/export/cc/class-imscc11.php
@@ -603,7 +603,7 @@ class Imscc11 extends Epub3 {
 	protected function zipImscc( $filename ) {
 		$zip = new \PclZip( $filename );
 
-		$list = $zip->create( $this->tmpDir, PCLZIP_OPT_NO_COMPRESSION, PCLZIP_OPT_REMOVE_ALL_PATH );
+		$list = $zip->create( $this->tmpDir . '/imsmanifest.xml', PCLZIP_OPT_NO_COMPRESSION, PCLZIP_OPT_REMOVE_ALL_PATH );
 		if ( 0 === absint( $list ) ) {
 			return false;
 		}
@@ -613,7 +613,9 @@ class Imscc11 extends Epub3 {
 			if ( ! $file->isFile() ) {
 				continue;
 			}
-
+			if ( 'imsmanifest.xml' === $file->getFilename() ) {
+				continue;
+			}
 			$files[] = $file->getPathname();
 		}
 
